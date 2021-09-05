5056521899b36b0f16eceaa7533cc7439f5a111b
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2021-09-05 17:27:45 -0500

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { actors } from '../data';
+const actorsData = actors.map(person => 
+  <div key={person.name}>
+    {person.name}
+      <ul>
+        <li>{person.movies}</li>
+      </ul>
+  </div>
+)
 
 const Actors = () => {
   return (
     <div>
       {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actorsData}
+
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import { movies } from '../data';
 
+const moviesData = movies.map(movie =>
+  <div key={movie.title}>
+      <h3>{movie.title}</h3>
+    <label>Time: </label>
+      {movie.time}
+    <br />
+    <br />
+     <label>Genres: </label>
+        {movie.genres.map(genre => <ul><li>{genre}</li></ul>)}
+  <br />
+  </div>
+
+)
+
 const Movies = () => {
   return (
     <div>
         {/*{code here}*/}
+        <h1>Movies Page</h1>
+          {moviesData}
     </div>
   );
 };
