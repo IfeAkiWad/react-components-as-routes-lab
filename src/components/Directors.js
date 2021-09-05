984f8dff6fe3e262ec2dac8ce69f57306261b858
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2021-09-05 17:27:21 -0500

    commit tree e5889999205ec92cd232aaf6dd68f2730efd0f7f

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { directors } from '../data';
 
+const directorData = directors.map( dir =>
+  <div key={dir.name}>
+    {dir.name}
+      <ul>
+        <li>{dir.movies}</li>
+      </ul>
+  </div>
+)
+
 const Directors = () => {
   return (
     <div>
       {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directorData}
     </div>
   );
 }
